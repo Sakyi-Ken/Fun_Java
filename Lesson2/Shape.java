a1de5f150af06c116e71547dbd73cabf38e439f7
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-09-09 09:18:26 +0000

    Add Shape class hierarchy with Circle and Rectangle implementations

---
+++ Lesson2/Shape.java
@@ -0,0 +1,35 @@
+
+abstract class Shape {
+
+    public abstract double getArea();
+}
+
+class Circle extends Shape {
+
+    private final double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Rectangle extends Shape {
+
+    private final double length;
+    private final double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double getArea() {
+        return length * width;
+    }
+}
